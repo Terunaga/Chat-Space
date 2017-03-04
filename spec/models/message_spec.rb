473fcd9c86f05b95b6c7ddf23f2e4103dbcc6eb6
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2017-03-05 01:40:55 +0900

    model test code for message

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    context 'with valid attributes' do
+      it 'is valid with all information' do
+        expect(build(:message)).to be_valid
+      end
+    end
+
+    context 'without valid attributes' do
+      it 'is invalid without text attribute' do
+        message = build(:message, text: nil)
+        message.valid?
+        expect(message.errors[:text]).to include 'を入力してください。'
+      end
+
+      it 'is invalid without group_id attribute' do
+        message = build(:message, group_id: nil)
+        message.valid?
+        expect(message.errors[:group_id]).to include 'を入力してください。'
+      end
+
+      it 'is invalid without user_id attribute' do
+        message = build(:message, user_id: nil)
+        message.valid?
+        expect(message.errors[:user_id]).to include 'を入力してください。'
+      end
+    end
+  end
 end
