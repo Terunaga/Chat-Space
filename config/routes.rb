da2e6db53af8fa9b688fd1f8e73082e3e90bfac1
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2017-03-04 15:34:50 +0900

    delete users_controller and use devise to update user information

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, only: [:registrations, :sessions]
   root 'chat_groups#index'
   resources :chat_groups, only: [:index] do
     collection { get 'index_message' }
   end
-  resources :users, only: [:edit, :update]
 end
