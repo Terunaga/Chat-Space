6a437a11c1e4b293b34074413a8b9b77bc631e07
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2017-03-04 11:46:14 +0900

    create temporary routings for message view

--- app/controllers/chat_groups_controller.rb
+++ app/controllers/chat_groups_controller.rb
@@ -1,4 +1,7 @@
 class ChatGroupsController < ApplicationController
   def index
   end
+
+  def index_message
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'chat_groups#index'
-  resources :chat_groups, only: [:index]
+  resources :chat_groups, only: [:index] do
+    collection { get 'index_message' }
+  end
 end
