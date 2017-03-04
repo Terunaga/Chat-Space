33432936b752915ea2f1d75cda40552406e49879
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2017-03-05 00:39:05 +0900

    define message controller to save messages

--- app/controllers/groups/messages_controller.rb
+++ app/controllers/groups/messages_controller.rb
@@ -1,6 +1,28 @@
 class Groups::MessagesController < ApplicationController
+  before_action :set_group, only: %i(index create)
+
   def index
-    @group  = Group.find(params[:group_id])
-    @groups = current_user.groups.order_by_desc
+    @groups   = current_user.groups.order_by_desc
+    @message  = Message.new
+    @messages = @group.messages.order_by_desc
+  end
+
+  def create
+    @message = current_user.messages.build(create_params)
+    if @message.save
+      redirect_to group_messages_path(@group), notice: 'Message was successfully posted.'
+    else
+      redirect_to group_messages_path(@group), alert: 'Message was unsuccessful.'
+    end
+  end
+
+  private
+
+  def set_group
+    @group   = Group.find(params[:group_id])
+  end
+
+  def create_params
+    params.require(:message).permit(:text).merge(group_id: params[:group_id])
   end
 end
