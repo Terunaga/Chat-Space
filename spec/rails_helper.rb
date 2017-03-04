0e3d170402bd28b46eba36ab749bf800d7c6ead9
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2017-03-04 17:14:25 +0900

    delete end causing syntax error

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,7 +34,6 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.include Devise::TestHelpers, type: :controller
   config.include ControllerMacros, type: :controller
-end
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
